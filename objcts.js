1d43a3ec1e670d93a3c8c6faf2c91fad32c02295
SeR4h <40196738+SeR4h@users.noreply.github.com>
2019-11-26 12:31:13 +0300

    2nd object modification

--- objcts.js
+++ objcts.js
@@ -2,9 +2,8 @@ let numbers = {}
 
 for (var number = 1; number <= 15; number++) {
   var myKey = number
-  var myValue = number ** 2
-  numbers[myKey] = myValue
-
+  var myKeySquared = number ** 2
+  numbers[myKey] = myKeySquared
 }
 
 console.log(numbers)
